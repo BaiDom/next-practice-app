d0eae7a66f76e13a09607bee7377c6c3c70c5635
BaiDom <dombaily88@gmail.com>
2025-10-27 08:32:11 +0000

    created json server and initial contact/user info

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-sm">
+      <div className="container mx-4 p-4 flex justify-between items center"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
